e35c996f783fe27f929197344388a6029211bc55
maciejisnotavailable <96336959+maciejisnotavailable@users.noreply.github.com>
2021-12-18 05:22:37 -0600

    Add files via upload

---
+++ cum.go
@@ -0,0 +1,37 @@
+package main
+import (
+        "fmt"
+        "time"
+)
+// This script generates many sperm cummy wummy inside of your personal computer, laptop, xbox, ps4, pet dog, jet engine, sausage machine, or your mom.
+func main() {
+    var cumMeter int = -1. // Defines the variables.
+    for cumMeter < 90 { // Begins the loop
+        cumMeter++
+        cumMeter++
+        cumMeter++
+        cumMeter++
+        cumMeter++
+        cumMeter++
+        cumMeter++
+        cumMeter++
+        cumMeter++
+        cumMeter++
+        cumMeter++
+        cumMeter++
+        cumMeter++
+        time.Sleep(2 * time.Second) // Sleep for 2 seconds.
+        fmt.Println("Generating CUM (semen) inside of the system... Percent completion: ", cumMeter, "%")}
+    time.Sleep(600 * time.Millisecond)
+    cumMeter++
+    cumMeter++
+    cumMeter++
+    cumMeter++
+    cumMeter++
+    cumMeter++
+    cumMeter++
+    cumMeter++
+    cumMeter++
+    cumMeter++
+    fmt.Println("Generating CUM (semen) inside of the system... Percent completion: ", cumMeter, "%")
+}
